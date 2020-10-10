1f68c52d30cca435c5b4b76ba3a8943b8a8247b0
Chris Phillipson <cphillipson@pivotal.io>
2020-10-10 16:02:14 -0700

    Add terraform/gcp/external-dns

---
+++ terraform/gcp/external-dns/main.tf
@@ -0,0 +1,25 @@
+module "external_dns" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules//external-dns/gcp"
+
+  domain_filter    = var.domain_filter
+  kubeconfig_path  = var.kubeconfig_path
+  gcp_project = var.gcp_project
+  gcp_service_account_credentials = var.gcp_service_account_credentials
+}
+
+variable "domain_filter" {
+  description = "An existing domain"
+}
+
+variable "kubeconfig_path" {
+  description = "The path to your .kube/config"
+  default = "~/.kube/config"
+}
+
+variable "gcp_project" {
+  description = "A Google Cloud Platform project id"
+}
+
+variable "gcp_service_account_credentials" {
+  description = "Path to service account credentials file in JSON format"
+}
