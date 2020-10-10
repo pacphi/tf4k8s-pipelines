7c0874d7c5873f9dacbb92185950a6ff884c03c2
Chris Phillipson <cphillipson@pivotal.io>
2020-10-10 16:22:29 -0700

    Add terraform/k8s/harbor

---
+++ terraform/k8s/harbor/main.tf
@@ -0,0 +1,36 @@
+module "harbor" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules/harbor"
+
+  domain = var.domain
+  ingress = var.ingress
+  kubeconfig_path = var.kubeconfig_path
+}
+
+variable "domain" {
+  description = "The base domain wherein harbor.<domain> will be deployed"
+}
+
+variable "ingress" {
+  description = "Used to specify which Ingress controller should serve a particular Ingress object.  Choices are: [ contour, nginx ]."
+}
+
+variable "kubeconfig_path" {
+  description = "The path to your .kube/config"
+  default = "~/.kube/config"
+}
+
+output "harbor_endpoint" {
+  description = "Harbor registry endpoint"
+  value       = "https://${module.harbor.harbor_domain}"
+}
+
+output "harbor_admin_username" {
+  description = "Harbor registry admin username"
+  value       = module.harbor.harbor_admin_username
+}
+
+output "harbor_admin_password" {
+  description = "Harbor registry admin password"
+  value       = module.harbor.harbor_admin_password
+}
+
