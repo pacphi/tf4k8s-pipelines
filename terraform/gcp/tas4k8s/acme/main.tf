4c35fda69d2123574bdb82f52b4af1230129f660
Chris Phillipson <cphillipson@pivotal.io>
2020-10-14 11:50:07 -0700

    Update  GCP tas4k8s pipeline definition
    * We now create certs and keys first with acme module before attempting to call tas4k8s module

---
+++ terraform/gcp/tas4k8s/acme/main.tf
@@ -0,0 +1,48 @@
+locals {
+  cf_domain = "tas.${var.base_domain}"
+}
+
+module "system_cert" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/gcp"
+
+  project = var.project
+  email = var.email
+  common_name = "*.${local.cf_domain}"
+  additional_domains = [ "*.login.${local.cf_domain}", "*.uaa.${local.cf_domain}" ]
+}
+
+module "workloads_cert" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/gcp"
+
+  project = var.project
+  email = var.email
+  common_name = "*.apps.${local.cf_domain}"
+}
+
+data "template_file" "certs_var_file" {
+  template = file("${path.module}/certs-and-keys.tpl")
+  
+  vars = {
+    system_fullchain_certificate = base64encode(module.system_cert.cert_full_chain)
+    system_private_key = base64encode(module.system_cert.cert_key)
+    workloads_fullchain_certificate = base64encode(module.workloads_cert.cert_full_chain)
+    workloads_private_key = base64encode(module.workloads_cert.cert_key)
+  }
+}
+
+resource "local_file" "certs_var_file" {
+  content  = data.template_file.certs_var_file.rendered
+  filename = "${path.module}/../certs.auto.tfvars"
+}
+
+variable "project" {
+  description = "A Google Cloud Platform project id"
+}
+
+variable "email" {
+  description = "Email address of base domain owner"
+}
+
+variable "base_domain" {
+   description = "An existing domain wherein a number of *.tas.<domain> wildcard domain recordsets will be made available"
+}
--- terraform/gcp/tas4k8s/main.tf
+++ terraform/gcp/tas4k8s/main.tf
@@ -1,74 +1,27 @@
-locals {
-  cf_domain = "tas.${var.base_domain}"
-}
-
-module "system_cert" {
-  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/gcp"
-
-  project = var.project
-  email = var.email
-  common_name = "*.${local.cf_domain}"
-  additional_domains = [ "*.login.${local.cf_domain}", "*.uaa.${local.cf_domain}" ]
-}
-
-module "workloads_cert" {
-  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/gcp"
-
-  project = var.project
-  email = var.email
-  common_name = "*.apps.${local.cf_domain}"
-}
-
-data "template_file" "certs_var_file" {
-  template = file("${path.module}/certs-and-keys.tpl")
-  
-  vars = {
-    system_fullchain_certificate = base64encode(module.system_cert.cert_full_chain)
-    system_private_key = base64encode(module.system_cert.cert_key)
-    workloads_fullchain_certificate = base64encode(module.workloads_cert.cert_full_chain)
-    workloads_private_key = base64encode(module.workloads_cert.cert_key)
-  }
-}
-
-resource "local_file" "certs_var_file" {
-  content  = data.template_file.certs_var_file.rendered
-  filename = "${path.module}/certs.auto.tfvars"
-}
-
 module "tas4k8s" {
   source = "git::https://github.com/pacphi/tf4k8s.git//modules/tas4k8s"
 
-  domain           = "tas.${var.base_domain}"
+  domain = "tas.${var.base_domain}"
 
-  registry_domain  = var.registry_domain
-  repository_prefix   = var.repository_prefix
-  registry_username     = var.registry_username
-  registry_password     = var.registry_password
+  registry_domain   = var.registry_domain
+  repository_prefix = var.repository_prefix
+  registry_username = var.registry_username
+  registry_password = var.registry_password
   
   pivnet_registry_hostname = var.pivnet_registry_hostname
-  pivnet_username     = var.pivnet_username
-  pivnet_password     = var.pivnet_password
+  pivnet_username          = var.pivnet_username
+  pivnet_password          = var.pivnet_password
 
-  remove_resource_requirements = var.remove_resource_requirements
-  add_metrics_server_components = var.add_metrics_server_components
-  enable_load_balancer = var.enable_load_balancer
-  use_external_dns_for_wildcard = var.use_external_dns_for_wildcard
-  enable_automount_service_account_token = var.enable_automount_service_account_token
+  remove_resource_requirements                   = var.remove_resource_requirements
+  add_metrics_server_components                  = var.add_metrics_server_components
+  enable_load_balancer                           = var.enable_load_balancer
+  use_external_dns_for_wildcard                  = var.use_external_dns_for_wildcard
+  enable_automount_service_account_token         = var.enable_automount_service_account_token
   metrics_server_prefer_internal_kubelet_address = var.metrics_server_prefer_internal_kubelet_address
-  use_first_party_jwt_tokens = var.use_first_party_jwt_tokens
+  use_first_party_jwt_tokens                     = var.use_first_party_jwt_tokens
 
   kubeconfig_path  = var.kubeconfig_path
   ytt_lib_dir      = var.ytt_lib_dir
-
-  certificate_variables_file_path = local_file.certs_var_file.filename
-}
-
-variable "project" {
-  description = "A Google Cloud Platform project id"
-}
-
-variable "email" {
-  description = "Email address of base domain owner"
 }
 
 variable "base_domain" {
