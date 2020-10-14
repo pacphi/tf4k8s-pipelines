456bef66e00e2b9b5669edc483cb314d18cdf70a
Chris Phillipson <cphillipson@pivotal.io>
2020-10-14 15:46:25 -0700

    Derp

--- terraform/gcp/tas4k8s/acme/main.tf
+++ terraform/gcp/tas4k8s/acme/main.tf
@@ -32,7 +32,7 @@ data "template_file" "certs_var_file" {
 
 resource "local_file" "certs_var_file" {
   content  = data.template_file.certs_var_file.rendered
-  filename = "/tmp/certs-and-keys.vars"
+  filename = "/tmp/build/put/pipeline-repo/terraform/gcp/tas4k8s/certs-and-keys.vars"
 }
 
 variable "project" {
