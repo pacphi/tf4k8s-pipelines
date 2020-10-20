a4b817a51a0c24862f57630b49512dfebfd02e83
Chris Phillipson <cphillipson@pivotal.io>
2020-10-19 23:55:20 -0700

    Initial support for tas4k8s install on EKS

---
+++ terraform/aws/tas4k8s/acme/main.tf
@@ -0,0 +1,63 @@
+locals {
+  cf_domain = "tas.${var.base_domain}"
+}
+
+module "system_cert" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/amazon"
+
+  dns_zone_id = var.dns_zone_id
+  email = var.email
+  common_name = "*.${local.cf_domain}"
+  additional_domains = [ "*.login.${local.cf_domain}", "*.uaa.${local.cf_domain}" ]
+}
+
+module "workloads_cert" {
+  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/amazon"
+
+  dns_zone_id = var.dns_zone_id
+  email = var.email
+  common_name = "*.apps.${local.cf_domain}"
+}
+
+data "template_file" "certs_var_file" {
+  template = file("${path.module}/certs-and-keys.tpl")
+  
+  vars = {
+    system_fullchain_certificate = trim(base64encode(module.system_cert.cert_full_chain), "\n")
+    system_private_key = trim(base64encode(module.system_cert.cert_key), "\n")
+    workloads_fullchain_certificate = trim(base64encode(module.workloads_cert.cert_full_chain), "\n")
+    workloads_private_key = trim(base64encode(module.workloads_cert.cert_key), "\n")
+  }
+}
+
+resource "local_file" "certs_var_file" {
+  content  = data.template_file.certs_var_file.rendered
+  filename = "/tmp/certs-and-keys.vars"
+}
+
+data "aws_s3_bucket" "certs_and_keys_bucket" {
+  bucket = "tf4k8s-pipelines-config"
+}
+
+resource "aws_s3_bucket_object" "certs_and_keys" {
+  bucket = data.aws_s3_bucket.certs_var_file.id
+  key    = var.path_to_certs_and_keys
+  source = local_file.certs_var_file.filename
+  etag = filemd5(local_file.certs_var_file.filename)
+}
+
+variable "email" {
+  description = "Email address of base domain owner"
+}
+
+variable "base_domain" {
+   description = "An existing domain wherein a number of *.tas.<domain> wildcard domain recordsets will be made available"
+}
+
+variable "path_to_certs_and_keys" {
+  description = "The path underneath the Amazon S3 bucket where the certs-and-keys.vars file will be stored."
+}
+
+variable "dns_zone_id" {
+  description = "The DNS zone identifier within Amazon Route53 that defines the base domain as an NS record."
+}
--- terraform/aws/tas4k8s/main.tf
+++ terraform/aws/tas4k8s/main.tf
@@ -1,72 +1,29 @@
-locals {
-  cf_domain = "tas.${var.base_domain}"
-}
-
-module "system_cert" {
-  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/amazon"
-
-  dns_zone_id = var.dns_zone_id
-  email = var.email
-  common_name = "*.${local.cf_domain}"
-  additional_domains = [ "*.login.${local.cf_domain}", "*.uaa.${local.cf_domain}" ]
-}
-
-module "workloads_cert" {
-  source = "git::https://github.com/pacphi/tf4k8s.git//modules/acme/amazon"
-
-  dns_zone_id = var.dns_zone_id
-  email = var.email
-  common_name = "*.apps.${local.cf_domain}"
-}
-
-resource "local_file" "certs_var_file" {
-  content = join(
-    "\n", [
-      "system_fullchain_certificate = ${base64encode(module.system_cert.cert_full_chain)}",
-      "system_private_key = ${base64encode(module.system_cert.cert_key)}",
-      "workloads_fullchain_certificate = ${base64encode(module.workloads_cert.cert_full_chain)}",
-      "workloads_private_key = ${base64encode(module.workloads_cert.cert_key)}"
-    ]
-  )
-  filename = "certs.auto.tfvars"
-}
-
 module "tas4k8s" {
-  source = "git::https://github.com/pacphi/tf4k8s.git//modules/tas4k8s"
+  source = "../../../modules/tas4k8s"
 
-  domain           = "tas.${var.base_domain}"
+  domain = "tas.${var.base_domain}"
 
-  registry_domain  = var.registry_domain
-  repository_prefix   = var.repository_prefix
-  registry_username     = var.registry_username
-  registry_password     = var.registry_password
+  registry_domain   = var.registry_domain
+  repository_prefix = var.repository_prefix
+  registry_username = var.registry_username
+  registry_password = var.registry_password
   
   pivnet_registry_hostname = var.pivnet_registry_hostname
-  pivnet_username     = var.pivnet_username
-  pivnet_password     = var.pivnet_password
+  pivnet_username          = var.pivnet_username
+  pivnet_password          = var.pivnet_password
 
-  remove_resource_requirements = var.remove_resource_requirements
-  add_metrics_server_components = var.add_metrics_server_components
-  enable_load_balancer = var.enable_load_balancer
-  use_external_dns_for_wildcard = var.use_external_dns_for_wildcard
-  enable_automount_service_account_token = var.enable_automount_service_account_token
+  remove_resource_requirements                   = var.remove_resource_requirements
+  add_metrics_server_components                  = var.add_metrics_server_components
+  enable_load_balancer                           = var.enable_load_balancer
+  use_external_dns_for_wildcard                  = var.use_external_dns_for_wildcard
+  enable_automount_service_account_token         = var.enable_automount_service_account_token
   metrics_server_prefer_internal_kubelet_address = var.metrics_server_prefer_internal_kubelet_address
-  use_first_party_jwt_tokens = var.use_first_party_jwt_tokens
+  use_first_party_jwt_tokens                     = var.use_first_party_jwt_tokens
 
   kubeconfig_path  = var.kubeconfig_path
   ytt_lib_dir      = var.ytt_lib_dir
 
-  depends_on = [ 
-    local_file.certs_var_file
-  ]
-}
-
-variable "dns_zone_id" {
-  description = "The id of an existing Route53 zone"
-}
-
-variable "email" {
-  description = "Email address of base domain owner"
+  certificate_variables_file_path = var.path_to_certs_and_keys
 }
 
 variable "base_domain" {
@@ -139,6 +96,10 @@ variable "kubeconfig_path" {
 variable "ytt_lib_dir" {
   description = "Path to directory where YAML template files will be operated upon by ytt k14s Teraform provider; @see https://github.com/k14s/terraform-provider-k14s/blob/master/docs/k14s_ytt.md"
   default = "../../ytt-libs"
+}
+
+variable "path_to_certs_and_keys" {
+  description = "The path underneath the Google Cloud Storage bucket where the certs-and-keys.vars file will be stored."
 }
 
 output "tas_api_endpoint" {
